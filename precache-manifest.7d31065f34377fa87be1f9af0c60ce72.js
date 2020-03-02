4f10ebb044b57eb257b31594aaf49efd327cdf4e
channaveerayya <channucs0009@gmail.com>
2020-03-02 11:22:12 +0530

    Updates

---
+++ precache-manifest.7d31065f34377fa87be1f9af0c60ce72.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274153632cc4d8e3d790f81e33e7577a",
+    "url": "/intugine-assignment/index.html"
+  },
+  {
+    "revision": "35d28820957121788ed1",
+    "url": "/intugine-assignment/static/css/3.32f6d7e9.chunk.css"
+  },
+  {
+    "revision": "5710dfb6d148706fe9e0",
+    "url": "/intugine-assignment/static/css/4.fd375434.chunk.css"
+  },
+  {
+    "revision": "fa18d6dae54c09959bbf",
+    "url": "/intugine-assignment/static/css/main.4e96f569.chunk.css"
+  },
+  {
+    "revision": "668e78b5e974f3a1bb56",
+    "url": "/intugine-assignment/static/js/2.16c6dd9c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/intugine-assignment/static/js/2.16c6dd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d28820957121788ed1",
+    "url": "/intugine-assignment/static/js/3.6412e2e7.chunk.js"
+  },
+  {
+    "revision": "5710dfb6d148706fe9e0",
+    "url": "/intugine-assignment/static/js/4.d57e17be.chunk.js"
+  },
+  {
+    "revision": "fa18d6dae54c09959bbf",
+    "url": "/intugine-assignment/static/js/main.184e39b1.chunk.js"
+  },
+  {
+    "revision": "87f9a4e554a540ff0dd6",
+    "url": "/intugine-assignment/static/js/runtime-main.fda253c5.js"
+  },
+  {
+    "revision": "85f9fdcc34b168fd80fa48babd0e1b99",
+    "url": "/intugine-assignment/static/media/destination.85f9fdcc.svg"
+  },
+  {
+    "revision": "877653aeb01976fd7edf924c661eacc2",
+    "url": "/intugine-assignment/static/media/logo.877653ae.svg"
+  },
+  {
+    "revision": "52b703a8be7195bf860d5e89cdc1ec6e",
+    "url": "/intugine-assignment/static/media/profile.52b703a8.svg"
+  },
+  {
+    "revision": "1f5bfd3e805919a486ffb8e1ab44785a",
+    "url": "/intugine-assignment/static/media/warehouse.1f5bfd3e.svg"
+  }
+]);
